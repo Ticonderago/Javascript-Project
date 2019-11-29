ad98bc284425e0ebfb6f112bd5bfe2aa23613a14
Ticonderago <nreinhardt1234@gmail.com>
2019-11-28 17:19:05 -0800

    added linewidth changing with slider

--- assets/functions.js
+++ assets/functions.js
@@ -8,36 +8,70 @@ if (localStorage.getItem("fP") === null) {
 // functions for the canvas
 
 const mainFunction = (htmlText) => {
+    var functionName = "";
+
     if (htmlText === "Draw") {
-        functionPicker = "1"
+        functionPicker = "1";
+        functionName = "Draw";
+        localStorage.setItem("Draw", functionName)
         localStorage.setItem("fP", functionPicker);
-        location.reload();
     }
+
+    if (htmlText === "Spiral") {
+        functionPicker = "2";
+        functionName = "Spiral";
+        localStorage.setItem("Spiral", functionName)
+        localStorage.setItem("fP", functionPicker);
+    }
+
+    if (htmlText === "Star") {
+        functionPicker = "3";
+        functionName = "Star";
+        localStorage.setItem("Star", functionName)
+        localStorage.setItem("fP", functionPicker);
+    }
+
+    location.reload();
 }
 
 const returnFunction = () => {
     if (localStorage.getItem("fP") === "0") {
-        const canvas = document.getElementById('canv');
-        const ctx = canvas.getContext('2d');
-
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50);
-
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(30, 30, 50, 50);
+        initial();
     }
 
     if (localStorage.getItem("fP") === "1") {
         draw();
     }
+
+    if (localStorage.getItem("fP") === "2") {
+        initial();
+    }
+
+    if (localStorage.getItem("fP") === "3") {
+        draw();
+    }
 }
 
 // drawing functions
+const initial = () => {
+    const canvas = document.getElementById('canv');
+    const ctx = canvas.getContext('2d');
+
+    ctx.lineWidth = localStorage.getItem("lineWidth");
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+}
 
 const draw = () => {
     const canvas = document.getElementById('canv');
     if (canv.getContext) {
         const ctx = canvas.getContext('2d');
+
+        ctx.lineWidth = localStorage.getItem("lineWidth");
         // Filled triangle
         ctx.beginPath();
         ctx.moveTo(25, 25);
