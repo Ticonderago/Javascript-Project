bcc8b2186b5602b9d02e07002ed89a284abbc319
Ticonderago <nreinhardt1234@gmail.com>
2019-12-09 14:44:25 -0800

    added drawing function for a star

--- assets/functions.js
+++ assets/functions.js
@@ -10,13 +10,13 @@ if (localStorage.getItem("fP") === null) {
 const mainFunction = (htmlText) => {
     var functionName = "";
 
-    if (htmlText === "Draw") {
+    if (htmlText === "Box") {
         functionPicker = "1";
-        functionName = "Draw";
-        localStorage.setItem("Draw", functionName)
+        functionName = "Box";
+        localStorage.setItem("Box", functionName)
         localStorage.setItem("fP", functionPicker);
         let output = document.getElementById("current-selection");
-        output.innerHTML = `Current Selection - ${functionName}`;
+        output.innerHTML = `Current Selection - ${localStorage.getItem("Box")}`;
     }
 
     if (htmlText === "Spiral") {
@@ -25,7 +25,7 @@ const mainFunction = (htmlText) => {
         localStorage.setItem("Spiral", functionName)
         localStorage.setItem("fP", functionPicker);
         let output = document.getElementById("current-selection");
-        output.innerHTML = `Current Selection - ${functionName}`;;
+        output.innerHTML = `Current Selection - ${localStorage.getItem("Spiral")}`;
     }
 
     if (htmlText === "Star") {
@@ -34,54 +34,90 @@ const mainFunction = (htmlText) => {
         localStorage.setItem("Star", functionName)
         localStorage.setItem("fP", functionPicker);
         let output = document.getElementById("current-selection");
-        output.innerHTML = `Current Selection - ${functionName}`;;
+        output.innerHTML = `Current Selection - ${localStorage.getItem("Star")}`;
     }
 }
 
 const returnFunction = () => {
     if (localStorage.getItem("fP") === "0") {
+        let output = document.getElementById("current-selection");
+        output.innerHTML = `Current Selection - none`;
         initial();
     }
 
     if (localStorage.getItem("fP") === "1") {
-        draw();
+        let output = document.getElementById("current-selection");
+        output.innerHTML = `Current Selection - ${localStorage.getItem("Box")}`;
+        box();
     }
 
     if (localStorage.getItem("fP") === "2") {
+        let output = document.getElementById("current-selection");
+        output.innerHTML = `Current Selection - ${localStorage.getItem("Spiral")}`;
         initial();
     }
 
     if (localStorage.getItem("fP") === "3") {
-        draw();
+        let output = document.getElementById("current-selection");
+        output.innerHTML = `Current Selection - ${localStorage.getItem("Star")}`;
+        star();
     }
 }
 
 // drawing functions
+
 const initial = () => {
     const canvas = document.getElementById('canv');
-    const ctx = canvas.getContext('2d');
 
-    ctx.lineWidth = localStorage.getItem("lineWidth");
-
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
-}
-
-const draw = () => {
-    const canvas = document.getElementById('canv');
     if (canv.getContext) {
         const ctx = canvas.getContext('2d');
 
         ctx.lineWidth = localStorage.getItem("lineWidth");
-        // Filled triangle
+        
+        // Non Filled Box
         ctx.beginPath();
-        ctx.moveTo(25, 25);
-        ctx.lineTo(105, 25);
-        ctx.lineTo(105, 105);
-        ctx.lineTo(25, 105)
+        ctx.moveTo(225, 225);
+        ctx.lineTo(305, 225);
+        ctx.lineTo(305, 305);
+        ctx.lineTo(225, 305)
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+const box = () => {
+    const canvas = document.getElementById('canv');
+
+    if (canv.getContext) {
+        const ctx = canvas.getContext('2d');
+
+        ctx.lineWidth = localStorage.getItem("lineWidth");
+        
+        // Non Filled Box
+        ctx.beginPath();
+        ctx.moveTo(225, 225);
+        ctx.lineTo(305, 225);
+        ctx.lineTo(305, 305);
+        ctx.lineTo(225, 305)
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+const star = () => {
+    const canvas = document.getElementById('canv');
+
+    if (canv.getContext) {
+        const ctx = canvas.getContext('2d');
+
+        ctx.lineWidth = localStorage.getItem("lineWidth");
+
+        // Non Filled Star
+        ctx.moveTo(235,230);
+        ctx.lineTo(270,310);
+        ctx.lineTo(190,265); 
+        ctx.lineTo(280,265);
+        ctx.lineTo(200,310);
         ctx.closePath();
         ctx.stroke();
     }
