e3d6a17eb644234afde011caf310db1a4ae3358d
Ticonderago <nreinhardt1234@gmail.com>
2020-01-21 14:06:22 -0800

    moved the refresh function into a seperate file so it can be ran after the html is rendered instead of in the function file before html is rendered to not break

---
+++ assets/refresh.js
@@ -0,0 +1,25 @@
+// if a refresh has occured do this
+if (localStorage.getItem("refresh") === "true") {
+    let setDrawingNum = localStorage.getItem("CurrD");
+    let button1 = document.getElementById("drawing-1");
+    let button2 = document.getElementById("drawing-2");
+    let button3 = document.getElementById("drawing-3");
+    if (setDrawingNum === "D1") {
+        button1.id = "dis-d1";
+        button1.disabled = true;
+    }
+    else if (setDrawingNum === "D2") {
+        button2.id = "dis-d2";
+        button2.disabled = true;
+    }
+    else if (setDrawingNum === "D3") {
+        button3.id = "dis-d3";
+        button3.disabled = true;
+    }
+    else {
+        let output = document.getElementById("current-drawing");
+        output.innerHTML = "No Drawing Selected!";
+        updateDrawInfo(localStorage.getItem("CurrD"));
+    }
+    localStorage.setItem("refresh", "false");
+}
